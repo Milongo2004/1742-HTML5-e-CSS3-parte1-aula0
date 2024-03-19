486e4bf0d37062103512b65aaf2a32ad530cae98
@Milongo2004 <camiloalvarez2004@gmail.com>
2024-03-19 16:05:02 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+function asignarTextoElemento(texto) {
+    let elementoHTML = document.getElementById('parrafo1'); //se importa como objeto que tiene y se le pueden asignar unos atributos
+    elementoHTML.innerHTML = texto;
+
+    return;
+}
+/*
+let nombreDeUsuario = prompt('digita tu nombre');
+asignarTextoElemento(`Producido por: ${nombreDeUsuario}`);
+*/
+/*
+valorando mi  futuro, 
+estnado seguro de poder progresar, 
+de llevar a cabo planes 
+de enfrentarse a los manes qeu lo quieren y que quiere.
+qué ventaja es no tirar paja,
+què's mucho mejor que haceer el amor?
+Confecciones Jaral Se puede salvar,
+ingreso:
+cédula(usuario),
+correo,
+contraseña,
+confirma contraseña;
+crear cuenta,
+verifica correo,
+selecciona rol:
+    trabajador,
+        consultar resultados    
+    jefe de planta,
+        crear/ seleccionar empresa
+        crear/seleccionar lote
+        crear / seleccionar operacion
+        
+
+
+
+*/
